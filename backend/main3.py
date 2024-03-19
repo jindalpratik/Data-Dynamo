50e3a6921db9a03d548296225c1f45e5b46d71b4
Pratik Jindal <117007380+jindalpratik@users.noreply.github.com>
2024-03-19 17:38:52 +0530

    Fix loader

---
+++ backend/main3.py
@@ -0,0 +1,37 @@
+import pathlib
+import textwrap
+
+import google.generativeai as genai
+
+from IPython.display import display
+from IPython.display import Markdown
+import PIL.Image
+
+
+# importing os module for environment variables
+import os
+# importing necessary functions from dotenv library
+from dotenv import load_dotenv, dotenv_values 
+# loading variables from .env file
+load_dotenv()
+ 
+# accessing and printing value
+GOOGLE_API_KEY=os.getenv("GOOGLE_API_KEY")
+
+def to_markdown(text):
+  text = text.replace('•', '  *')
+  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+
+genai.configure(api_key=GOOGLE_API_KEY)
+
+img = PIL.Image.open(r".\output_CjnS0vNTsVis_0.png")
+
+model = genai.GenerativeModel('gemini-pro-vision')
+
+response = model.generate_content(img)
+
+print(response.text)
+
+
+
+
